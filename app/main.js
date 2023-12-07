8d6a2d4e245ee5272efc2f930e0527f632bdeae1
AbdielMamani <abdielmamani@upeu.edu.pe>
2023-12-07 12:48:15 -0500

    solving renderer problem and content Security policy

--- app/main.js
+++ app/main.js
@@ -1,14 +1,18 @@
 const {app, BrowserWindow}= require ('electron');
+const path = require("path");
+
 let mainWindow = null
 
 app.on('ready',()=>{
     mainWindow = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: true
+            contextIsolation: true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'renderer.js')
         }
-    })
+    });
     mainWindow.loadFile(__dirname + '/index.html');
     mainWindow.on('closed',()=>{
         mainWindow =null;
-    })
+    });
 })
